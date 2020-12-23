b54786006df07fb49436d659fa292c30a4966e5f
asifhaider995 <asif.haider995@gmail.com>
2020-12-23 11:30:29 +0600

    init commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Navbar() {
+    return (
+        <div>
+            This is a navbar
+        </div>
+    )
+}
+
+export default Navbar;
---
+++ src/sections/TopSection.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function TopSection(props) {
+    return (
+        <div>
+            { props.content }
+        </div>
+    )
+}
+
+export default TopSection;
